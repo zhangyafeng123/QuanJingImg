93ed804fef9f7f8d579eeddb7d86b6a94cbf35b7
zhangyafeng123 <1240334911@qq.com>
2017-11-21 15:48:05 +0800

    Initial Commit

---
+++ panoramaGLDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  panoramaGLDemo
+//
+//  Created by linjianguo on 2017/11/21.
+//  Copyright © 2017年 com.justsee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ panoramaGLDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  panoramaGLDemo
+//
+//  Created by linjianguo on 2017/11/21.
+//  Copyright © 2017年 com.justsee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
